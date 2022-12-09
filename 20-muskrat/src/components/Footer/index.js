bc64b1828ff8b00ce768dcc3c919748f35105739
Mona Lisa <naomiroznik@gmail.com>
2022-12-09 13:48:35 -0500

    adding the header and footer

--- 20-muskrat/src/components/Footer/index.js
+++ 20-muskrat/src/components/Footer/index.js
@@ -1,1 +1,20 @@
 import React from "react";
+
+function Footer() {
+    return (
+      <footer>
+        <h2>Naomi R</h2>
+        <p>
+          <ul>
+            <a href="https://github.com/n-roz">
+              <li className="logo">
+                <VscGithubAlt />
+              </li>
+            </a>
+          </ul>
+        </p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
---
+++ 20-muskrat/src/components/Header/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Nav from "../Nav";
+
+function Header(props) {
+  const { currentTab, handleTabChange } = props;
+
+  return (
+    <div>
+      <section>
+        <header className="flex-row px-1">
+          <div>
+            <h1>Naomi's Portfolio</h1>
+          </div>
+          <div>
+            <Nav
+              currentTab={currentTab}
+              handleTabChange={handleTabChange}
+            ></Nav>
+          </div>
+        </header>
+      </section>
+      <section class="hero">
+        <div class="hero-cta">
+          <h2>Welcome</h2>
+          <p>
+            Welcome to Naomi's portfolio.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Header;
