65ad50efc1681633fd597888adbec458b7c14bc5
Mona Lisa <naomiroznik@gmail.com>
2022-12-09 13:35:35 -0500

    starting on navbar

--- 20-muskrat/src/components/Nav/index.js
+++ 20-muskrat/src/components/Nav/index.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import Nav from './components/Nav';
-import About from './components/About';
+import React from "react";
 
-function App() {
-  return (
-    <div>
-      <Nav></Nav>
-      <main>
-        <About></About>
-      </main>
-    </div>
-  );
+function Nav() {
+    const categories = [
+        {
+          name: "About",
+          description:
+            "Me",
+        },
+        { name: "Portfolio", description: "Portraits of people in my life" },
+        { name: "food", description: "Delicious delicacies" },
+        {
+          name: "Contact",
+          description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+        },
+      ];
+    return(
+        <div className="navbar">
+            <ol>
+                <li className="tab">
+                    <a href="/#About Me">
+                        //before state
+                    </a>
+                </li>
+                <li className="tab">
+                    <a href="/#Portfolio">
+                        //before state
+                    </a>
+                </li>
+                <li className="tab">
+                    <a href="/#Contact">
+                        //before state
+                    </a>
+                </li>
+                <li className="tab">
+                    <a href="/#Resume">
+                        Resume
+                    </a>
+                </li>
+            </ol>
+        </div>
+    )
 }
 
-export default App;
+export default Nav;
