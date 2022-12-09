ccbae2ccd7ba734f5645a2e2c6b40ade20d0c92f
Mona Lisa <naomiroznik@gmail.com>
2022-12-09 15:35:55 -0500

    starting on the contact form

---
+++ 20-muskrat/src/components/Contact/index.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+
+import { validateEmail } from './utils/helpers';
+
+function Contact() {
+    const [email, setEmail] = useState("");
+    const [userName, setUserName] = useState("");
+    const [message, setMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const handleInputChange = (e) => {
+        //     e.preventDefault();
+        //     if (!errorMessage) {
+        //       console.log('Submit Form', formState);
+        //     }
+        //   };
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        if (inputType === "email") {
+            setEmail(inputValue);
+        } else if (inputType === "userName") {
+            setUserName(inputValue);
+        } else {
+            setMessage(inputValue);
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if (!validateEmail(email) || !userName) {
+            setErrorMessage("Email or Name is invalid");
+            return;
+        }
+
+        if (!setMessage(message)) {
+            setErrorMessage(`Message is required.`);
+            return;
+        }
+
+        setUserName("");
+        setMessage("");
+        setEmail("");
+    };
+
+    //     if (e.target.name === 'email') {
+    //         const isValid = validateEmail(e.target.value);
+    //         if (!isValid) {
+    //             setErrorMessage('Your email is invalid.');
+    //         } else {
+    //             setErrorMessage('');
+    //         }
+    //     } else {
+    //         if (!e.target.value.length) {
+    //             setErrorMessage(`${e.target.name} is required.`);
+    //         } else {
+    //             setErrorMessage('');
+    //         }
+    //     }
+    //     if (!errorMessage) {
+    //         setFormState({ ...formState, [e.target.name]: e.target.value });
+    //         console.log('Handle Form', formState);
+    //     }
+    // };
+
+    return (
+        <section>
+            <h1 data-testid="h1tag">Contact me</h1>
+            <form id="contact-form" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+                </div>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+                <button data-testid="button" type="submit">Submit</button>
+            </form>
+        </section>
+    );
+}
+
+export default ContactForm;
