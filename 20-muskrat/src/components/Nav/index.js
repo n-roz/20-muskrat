b5ecef8b15da5ba0d8cd9c4a663c8070501f76ec
Mona Lisa <naomiroznik@gmail.com>
2022-12-09 15:22:55 -0500

    working on app.js and nav

--- 20-muskrat/src/components/Nav/index.js
+++ 20-muskrat/src/components/Nav/index.js
@@ -1,45 +1,57 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Nav() {
-    const categories = [
-        {
-          name: "About",
-          description:
-            "Me",
-        },
-        { name: "Portfolio", description: "Portraits of people in my life" },
-        { name: "food", description: "Delicious delicacies" },
-        {
-          name: "Contact",
-          description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-        },
-      ];
-    return(
-        <div className="navbar">
-            <ol>
-                <li className="tab">
-                    <a href="/#About Me">
-                        //before state
-                    </a>
-                </li>
-                <li className="tab">
-                    <a href="/#Portfolio">
-                        //before state
-                    </a>
-                </li>
-                <li className="tab">
-                    <a href="/#Contact">
-                        //before state
-                    </a>
-                </li>
-                <li className="tab">
-                    <a href="/#Resume">
-                        Resume
-                    </a>
-                </li>
-            </ol>
-        </div>
-    )
+function Nav(props) {
+  const {
+    categories = [],
+    setCurrentCategory,
+    contactSelected,
+    currentCategory,
+    setContactSelected,
+  } = props;
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
+
+  return (
+    <header className="flex-row px-1">
+      <h2>
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
+              About me
+            </a>
+          </li>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
+          </li>
+          {categories.map((category) => (
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && !contactSelected && 'navActive'
+                }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category);
+                  setContactSelected(false);
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Nav;
